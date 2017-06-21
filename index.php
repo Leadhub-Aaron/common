cf4058d8745595f44a7cda41410bcdfa0f72294c
Richard <richard@leadhub.net>
2017-06-21 21:06:55 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Plugin Name: LEADHUB Developer Kit
+ * Plugin URI: http://www.leadhub.net/
+ * Description: Common utilities used by LEADHUB themes and landing pages.
+ * Author: Leadhub
+ * Author URI: http://www.leadhub.net/
+ * License: GPLv2 or later
+ */
+
+if(!class_exists('Leadhub\\Common')) {
+    include __DIR__ . '/class_leadhub_common.php';
+});
